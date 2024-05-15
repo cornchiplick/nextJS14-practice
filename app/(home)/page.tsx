cfe6954232e0051320c656be49a0bd88b16c6465
cornchiplick <ilsunkiss@gmail.com>
2024-05-15 17:03:24 +0900

    Fix: data fetching 시간이 길어질 경우 loading.tsx 를 활용하는 방법 - 파일 이름은 반드시 loading 이어야 한다

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 const getMovies = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(URL);
   const json = await response.json();
   return json;
