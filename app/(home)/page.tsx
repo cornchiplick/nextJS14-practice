9034678ac8f04a1ce8baf0f3d25062af31ef8ddb
cornchiplick <ilsunkiss@gmail.com>
2024-05-15 17:35:38 +0900

    Fix: Promise all 을 활용한 다중 data fetching 병렬화

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,22 @@
-"use client";
+import Link from "next/link";
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 const getMovies = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  const response = await fetch(URL);
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 };
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
