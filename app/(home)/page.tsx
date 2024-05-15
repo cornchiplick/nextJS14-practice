16e865eef950de586e03a66ba62ccc72aff96906
cornchiplick <ilsunkiss@gmail.com>
2024-05-15 16:47:40 +0900

    Refact: nextjs 방식의 data fetching 초기버전

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,14 @@
 "use client";
-import {useEffect, useState} from "react";
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-  useEffect(() => {
-    getMovies();
-  }, []);
+const getMovies = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+};
 
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
