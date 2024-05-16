f9e3a240202bf152fcfeeadfe54f644ec0424e45
cornchiplick <ilsunkiss@gmail.com>
2024-05-16 23:28:43 +0900

    Refact: 여러 data fetch 코드를 두 개의 컴포넌트로 분리하여 각각 Suspense 로 감싸줌

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,16 +1,16 @@
-import {API_URL} from "@myapp/app/(home)/page";
-
-async function getMovie(id: string) {
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
+import MovieInfo from "@myapp/components/movie-info";
+import MovieVideos from "@myapp/components/movie-videos";
+import {Suspense} from "react";
 
 export default async function MovieDetail({params: {id}}: {params: {id: string}}) {
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  return <h1>{movie.title}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
