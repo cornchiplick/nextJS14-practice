f5fd60795857faffd8518e545afcdae2b2d24045
cornchiplick <ilsunkiss@gmail.com>
2024-05-18 12:45:30 +0900

    Style: home, movie 페이지 스타일 변경, movie 컴포넌트 분리

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Movie from "@myapp/components/movie";
+import styles from "@myapp/styles/home.module.css";
 
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
@@ -11,11 +12,9 @@ const getMovies = async () => {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title} />
       ))}
     </div>
   );
